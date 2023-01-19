6423df09e1b98fe67abb4c40a9186c5e27b93c33
Gearboxpetrolhead <sandrinemalanga2001@gmail.com>
2023-01-19 17:17:12 +0300

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
+# require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  resources :rides
-  resources :users
+  # authenticate :user, lambda { |u u.admin? } do
+  #   mount Sidekiq ::web => '/sidekiq'
+  # end
+
+  # devise_for :users
+  resources :rides, :users, :layouts
+
   get 'welcome/index'
+
+  get 'users/:id', to: 'users#show'
+
+  get 'rides/:id' , to: 'rides#show'
+
+  
+  
+  get 'exit', to: 'sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root to: 'welcome#index'
+
 end
